88c4a3bdbe79e29bf538e46297857468e3c6bac1
Quinton Fults <coding.banjo@gmail.com>
2020-06-19 13:25:55 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Form from './components/Form'
+import Movie from './components/Movie'
+import axios from 'axios'
 
-const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  )
+class App extends Component {
+
+  state = {
+    title: '',
+    movie: {}
+  }
+
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSearchMovie = event => {
+    event.preventDefault()
+    console.log(this.state.title)
+    axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${this.state.title}`)
+      .then(({ data }) => {
+        this.setState({ movie: data, title: '' })
+      })
+      .catch(err => console.error(err))
+  }
+
+  render() {
+    return (
+      <>
+        <Form
+          title={this.state.title}
+          handleInputChange={this.handleInputChange}
+          handleSearchMovie={this.handleSearchMovie} />
+        {this.state.movie.Title ? <Movie movie={this.state.movie} /> : null}
+      </>
+    )
+  }
 }
 
 export default App
---
+++ src/components/Form/Form.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Form = props => {
+  return (
+    <form>
+      <p>
+        <label htmlFor="title">title</label>
+        <input 
+          type="text" 
+          name="title" 
+          id="title"
+          onChange={props.handleInputChange}
+          value={props.title} />
+      </p>
+      <p>
+        <button onClick={props.handleSearchMovie}>Search Movie</button>
+      </p>
+    </form>
+  )
+}
+
+export default Form
